9420b6685c068606e3d308fcd4a2f09f1c683564
denwelr <85834107+denwelr@users.noreply.github.com>
2021-07-02 16:29:52 +0400

    Загрузка

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+    $(".header__burger").click(function(){
+        $(this).toggleClass("active");
+        $(".header__inner").toggleClass("active");
+    });
+
+    var $page = $('html, body');
+    $('.header__link').click(function() {
+
+        if($(".header__burger").hasClass("active")) {
+            $(this).removeClass("active");
+            $(".header__inner").removeClass("active");
+        }
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+});
